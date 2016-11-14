defb0f4549e9335708e815e7bfe7757800dfd1da
Roger Fernandez <rogerfernandezv@gmail.com>
2016-11-14 00:30:53 -0200

    task prototyping using sqlite

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,26 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::group(['middleware' => 'api'], function(){
+    Route::get('tasks', function(){
+        return App\Task::latest()->orderBy('created_at','desc')->get();
+    });
+
+    Route::get('task/{id}', function($id){
+        return App\Task::findOrFail($id);
+    });
+
+    Route::post('task/store', function(Request $request){
+        return App\Task::create(['body' => $request->input(['body'])]);
+    });
+
+    Route::patch('task/{id}', function(Request $request, $id){
+        App\Task::findOrFail($id)->update(['body' => $request->input(['body'])]);
+    });
+
+    Route::delete('task/{id}', function($id){
+        return App\Task::destroy($id);
+    });
+
+});
